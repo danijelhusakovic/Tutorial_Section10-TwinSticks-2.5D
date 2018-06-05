7826780d38dcab049f8e3d429e09e6eaec0fc151
danijel <danijel.husakovic@gmail.com>
2018-06-05 14:32:42 +0200

    Added a prefab ball and a long box (beam) with a box on it with a rigidbody and small weight. By default the gameplay is being recorded in the last 100 frames, and by holding the Fire button (click or ctrl), last 100 frames play back. The issue is if the buffer is bigger(1000 frames) and we start playback before all the frames have recorded. . .it just stays there until it cycles back to the start.

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame [bufferFrames];
+	private Rigidbody rigidBody;
+	private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		gameManager = GameObject.FindObjectOfType<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameManager.isRecording) {
+			Record ();
+		} else {
+			Playback ();
+		}
+
+	}
+
+	void Playback() {
+		rigidBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		Debug.Log ("Reading frame: " + frame);
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+
+	void Record ()
+	{
+		rigidBody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		print ("Writing frame: " + frame);
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+}
+
+/// <summary>
+/// A structure for storing time, rotation and position.
+/// </summary>
+public struct MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame(float time, Vector3 pos, Quaternion rot){
+		frameTime = time;
+		position = pos;
+		rotation = rot;
+	}
+}
